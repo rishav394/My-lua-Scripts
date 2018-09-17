93cfba73ff83d282aea9ae2110fa2c506830b908
Rishav Rungta <rishav394@gmail.com>
2018-09-17 12:50:37 +0530

    Fix some issues with ordering

--- Bounty Rune.lua
+++ Bounty Rune.lua
@@ -11,10 +11,10 @@ Rune.InsertData("Alert")
 Rune.InsertData("ChatWheel + Alert")
 
 
-Rune.OverAll = Menu.AddOption({ "Utility", "Bounty Rune Notification" }, "Overall Enabled", "Turn the script on or off (RECOMMENDED: ON)")
-Rune.MyOptions = Menu.AddOption({ "Utility", "Bounty Rune Notification" }, "Mode", "Set the notification Mode  (RECOMMENDED: CHATWHEEL + ALERT)", 1, #Rune.Options, 1)
-Rune.Time = Menu.AddOption({"Utility", "Bounty Rune Notification"}, "Notification at (time in seconds)", "The time tick in seconds in which the Notification wil be sent  (RECOMMENDED: 50)", 30, 58, 2)
-Rune.DisableTime = Menu.AddOption({"Utility", "Bounty Rune Notification"}, "Time in minutes to stop script", "The minute mark at which the script auto disables  (RECOMMENDED: 35)", 25, 60, 5)
+Rune.OverAll = Menu.AddOption({ "Utility", "Bounty Rune Notification" }, "1. Overall Enabled", "Turn the script on or off (RECOMMENDED: ON or else wont work)")
+Rune.MyOptions = Menu.AddOption({ "Utility", "Bounty Rune Notification" }, "2. Mode", "Set the notification Mode  (RECOMMENDED: CHATWHEEL + ALERT)", 1, #Rune.Options, 1)
+Rune.Time = Menu.AddOption({"Utility", "Bounty Rune Notification"}, "3. Notification at (time in seconds)", "The time tick in seconds in which the Notification wil be sent  (RECOMMENDED: 50)", 30, 58, 2)
+Rune.DisableTime = Menu.AddOption({"Utility", "Bounty Rune Notification"}, "4. Time in minutes to stop script", "The minute mark at which the script auto disables  (RECOMMENDED: 35)", 25, 60, 5)
 
 for i, v in ipairs(Rune.Options) do
     Menu.SetValueName(Rune.MyOptions, i, v.name)
